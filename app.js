251ed3388e238d26713222b65f798225c4b41c25
Parul Priya <parulpriya30@tamu.edu>
2017-02-13 12:18:20 -0600

    Add delete route

--- app.js
+++ app.js
@@ -82,8 +82,19 @@ app.put("/blogs/:id",function(req,res){
             res.redirect("/blogs/" + updatedBlog._id);
         }
     })
-    
 })
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req,res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            console.log(err)
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+})
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog Server Running!");
 })
